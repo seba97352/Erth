29c140cfc78b9151c125f4ccfd4a34ccff0f7c46
Vojtěch Šebek <sebekvo@gmail.com>
2016-09-06 00:37:16 +0200

    Init with Alamofire

---
+++ Podfile
@@ -0,0 +1,6 @@
+# platform :ios, '9.0'
+  use_frameworks!
+
+target 'Earth' do
+ pod 'Alamofire', '~> 3.4'
+end
---
+++ Pods/Target Support Files/Pods-Earth/Pods-Earth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Earth : NSObject
+@end
+@implementation PodsDummy_Pods_Earth
+@end
