1d0b7300e80d7cabc52b965bc0f5fb699b342d82
Vojtěch Šebek <sebekvo@gmail.com>
2016-09-06 00:27:04 +0200

    Initial Commit

---
+++ Earth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Earth
+//
+//  Created by Vojtěch Šebek on 06.09.16.
+//  Copyright © 2016 Vojtěch Šebek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
