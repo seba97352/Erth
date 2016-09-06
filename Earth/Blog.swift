e2f8ef81c6d1a60aea0c0fc571f2be66589f29c6
Vojtěch Šebek <sebekvo@gmail.com>
2016-09-06 19:09:52 +0200

    JSON to object implemented

---
+++ Earth/Blog.swift
@@ -0,0 +1,27 @@
+//
+//  Blog.swift
+//  Earth
+//
+//  Created by Vojtěch Šebek on 06.09.16.
+//  Copyright © 2016 Vojtěch Šebek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class blog {
+    let idBlog: Int
+    let dateBlog: String
+    let partTextBlog: String
+    let textBlog: String
+    
+    init (idBlog: Int, dateBlog: String, partTextBlog: String, textBlog: String) {
+        self.idBlog = idBlog
+        self.dateBlog = dateBlog
+        self.partTextBlog = partTextBlog
+        self.textBlog = textBlog
+    }
+    
+    
+}
--- Earth/ViewController.swift
+++ Earth/ViewController.swift
@@ -10,12 +10,48 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myImage: UIImageView!
 
+    var blogs = [blog]()
+    var jsonArray: NSMutableArray?
+    let bobovkaURL = "http://becomehero.eu/public/api.php/blogs?transform=1?filter[]=idBlog,le,3&filter[]=idBlog,ge,1,transform=1"
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        Alamofire.request(.GET, bobovkaURL, encoding:.JSON).responseJSON
+            { response in switch response.result {
+            case .Success(let JSON):
+                
+                let response = JSON as! NSDictionary
+                let thisBlogs = response.objectForKey("blogs") as! NSMutableArray
+                
+                for item in thisBlogs {
+                    let idBlog = item["idBlog"] as! String
+                    let partTextBlog = item["partTextBlog"] as! String
+                    let textBlog = item["textBlog"] as! String
+                    let dateBlog = item["dateBlog"] as! String
+                    
+                    let newBlog = blog(idBlog: Int(idBlog)!, dateBlog: dateBlog, partTextBlog: partTextBlog, textBlog: textBlog)
+                    self.blogs.append(newBlog)
+                }
+                
+                print(self.blogs.count)
+                
+            case .Failure(let error):
+                print("Request failed with error: \(error)")
+                }
+        }
+        
+        
+        
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
